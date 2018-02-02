f57aae1a3b09a232092411e1143a9b2645a81aa0
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2018-02-02 09:19:10 -0400

    Avance de clases demo intents

--- AND_401_08_Intents/app/src/main/java/com/miramicodigo/intents/SegundaActivity.java
+++ AND_401_08_Intents/app/src/main/java/com/miramicodigo/intents/SegundaActivity.java
@@ -1,17 +1,50 @@
 package com.miramicodigo.intents;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class SegundaActivity extends AppCompatActivity {
 
+    TextView tvTextoUno, tvTextoDos;
+    EditText etCajaTexto;
+    Button btnRespuesta;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
 
+        tvTextoUno = (TextView) findViewById(R.id.tvTextoUno);
+        tvTextoDos = (TextView) findViewById(R.id.tvTextoDos);
+        etCajaTexto = (EditText) findViewById(R.id.etCajaTexto);
+        btnRespuesta = (Button) findViewById(R.id.btnRespuesta);
+
+        Bundle bundle = getIntent().getExtras();
+
+        try {
+            tvTextoUno.setText(bundle.getString("valor1", ""));
+            tvTextoDos.setText(bundle.getString("valor2", ""));
+        }catch (Exception e) {
+        }
+
+        try {
+            etCajaTexto.setText(bundle.getString("valor3", ""));
+        } catch (Exception e) {
+        }
+
+        btnRespuesta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.putExtra("respuesta", etCajaTexto.getText().toString());
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
     }
 }
