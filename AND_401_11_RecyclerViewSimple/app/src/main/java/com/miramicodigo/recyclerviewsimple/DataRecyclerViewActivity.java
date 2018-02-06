3409ed102c2d846fd348b8ba5dc07a9e06b6d364
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2018-02-06 09:18:43 -0400

    Demo RecyclerViewSimple

--- AND_401_11_RecyclerViewSimple/app/src/main/java/com/miramicodigo/recyclerviewsimple/DataRecyclerViewActivity.java
+++ AND_401_11_RecyclerViewSimple/app/src/main/java/com/miramicodigo/recyclerviewsimple/DataRecyclerViewActivity.java
@@ -2,9 +2,12 @@ package com.miramicodigo.recyclerviewsimple;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,8 +23,14 @@ public class DataRecyclerViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_recycler_view);
 
+        recyclerView = (RecyclerView) findViewById(R.id.rvDatos);
+        recyclerView.setLayoutManager(
+                new LinearLayoutManager(getApplicationContext()));
+
         listaDatos = llenarDatos();
 
+        adaptador = new RecyclerViewAdapter(listaDatos);
+        recyclerView.setAdapter(adaptador);
     }
 
     public List<String> llenarDatos() {
@@ -33,10 +42,11 @@ public class DataRecyclerViewActivity extends AppCompatActivity {
     }
 
     private class RecyclerViewHolder extends RecyclerView.ViewHolder {
+        private TextView tvTexto;
 
         public RecyclerViewHolder(View itemView) {
             super(itemView);
-
+            tvTexto = (TextView) itemView;
         }
     }
 
@@ -49,12 +59,16 @@ public class DataRecyclerViewActivity extends AppCompatActivity {
 
         @Override
         public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return null;
+            LayoutInflater layoutInflater = LayoutInflater.from(getApplicationContext());
+            View view = layoutInflater.inflate(android.R.layout.simple_list_item_1,
+                    parent, false);
+            return new RecyclerViewHolder(view);
         }
 
         @Override
         public void onBindViewHolder(RecyclerViewHolder holder, int position) {
-
+            String dato = data.get(position);
+            holder.tvTexto.setText(dato);
         }
 
         @Override
