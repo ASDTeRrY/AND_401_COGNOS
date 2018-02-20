2dd8915c30e58b5cae73f2e1e28723bc5882b28d
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2018-02-20 09:08:10 -0400

    Codigo base demo SoundPool

---
+++ AND_401_28_SoundPool/app/src/main/java/com/miramicodigo/soundpool/MainActivity.java
@@ -0,0 +1,61 @@
+package com.miramicodigo.soundpool;
+
+import android.annotation.TargetApi;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SoundPool spIdiota, spCallate, spVeteAlDiablo;
+    private int resIdiota, resCallate, resVeteAlDiablo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        createSoundPool();
+    }
+
+    private void createSoundPool() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            createNewSoundPool();
+        } else {
+            createOldSoundPool();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    protected void createNewSoundPool(){
+
+        chargeSoundPool();
+    }
+
+    @SuppressWarnings("deprecation")
+    protected void createOldSoundPool(){
+
+        chargeSoundPool();
+    }
+
+
+    public void chargeSoundPool() {
+
+    }
+
+    public void idiota(View v) {
+
+    }
+
+    public void callate(View v) {
+
+    }
+
+    public void veteAlDiablo(View v) {
+
+    }
+}
