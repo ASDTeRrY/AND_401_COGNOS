830e9c2f32d1918436b61c9791c02e6aa9e93bb2
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2018-02-27 09:11:33 -0400

    Codigo base demo AsyncTask

---
+++ AND_401_35_AsyncTask/app/src/main/java/com/miramicodigo/asynctask/MainActivity.java
@@ -0,0 +1,33 @@
+package com.miramicodigo.asynctask;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.tvResultado);
+    }
+
+    public void onClick(View view) {
+
+    }
+
+}
