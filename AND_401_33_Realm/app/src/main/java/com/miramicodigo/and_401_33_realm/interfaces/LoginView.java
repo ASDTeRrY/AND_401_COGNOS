10bc497bc6184bb1ec97e073b542bceb33572200
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2018-02-28 01:58:28 -0400

    Demo basico Realm

---
+++ AND_401_33_Realm/app/src/main/java/com/miramicodigo/and_401_33_realm/interfaces/LoginView.java
@@ -0,0 +1,11 @@
+package com.miramicodigo.and_401_33_realm.interfaces;
+
+public interface LoginView {
+
+    void goToRegister();
+    void fieldEmpty();
+    void fieldNotEmpty();
+    void verifyField();
+    void success();
+    void failure();
+}
---
+++ AND_401_33_Realm/app/src/main/java/com/miramicodigo/and_401_33_realm/presenter/LoginPresenter.java
@@ -0,0 +1,53 @@
+package com.miramicodigo.and_401_33_realm.presenter;
+
+import com.miramicodigo.and_401_33_realm.interfaces.LoginView;
+import com.miramicodigo.and_401_33_realm.R;
+import com.miramicodigo.and_401_33_realm.model.User;
+
+import java.util.Collection;
+
+import io.realm.Realm;
+
+public class LoginPresenter {
+
+    private LoginView loginView;
+    private Boolean sucessLogin;
+
+    public LoginPresenter(LoginView loginView) {
+        this.loginView = loginView;
+    }
+
+    public void verifyButton(int oneButton) {
+        switch (oneButton) {
+            case R.id.btnMainLogin:
+                loginView.verifyField();
+                break;
+            case R.id.btnMainRegister:
+                loginView.goToRegister();
+                break;
+        }
+    }
+
+    public void verifyFieldEmpty(String user, String password) {
+        if (user.isEmpty() || password.isEmpty()) {
+            loginView.fieldEmpty();
+        } else {
+            loginView.fieldNotEmpty();
+        }
+    }
+
+    public void queryUser(Realm realm, String userName, String password) {
+        sucessLogin = Boolean.FALSE;
+        final Collection<User> users = realm.where(User.class).findAll();
+        for (User user : users) {
+            if (user.getUser().equals(userName) && user.getPassword().equals(password)) {
+                sucessLogin = Boolean.TRUE;
+            }
+        }
+        if (sucessLogin) {
+            loginView.success();
+        } else {
+            loginView.failure();
+        }
+    }
+}
